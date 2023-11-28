b41d35389b62c3e86a400a2f6ed82a8caecf9244
fcesnak <70899695+fcesnak@users.noreply.github.com>
2023-11-28 10:12:45 +0100

    feat: incremental processing and primary keys

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from keboola.component.base import ComponentBase 
+from keboola.component.base import ComponentBase
 from keboola.component import UserException
 
 from ttd import TradeDesk
@@ -10,16 +10,19 @@ class Component(ComponentBase):
 
     def __init__(self):
         super().__init__()
-    
+
     def run(self):
         params = self.configuration.parameters
         logging.info(params)
-        ttd_client = TradeDesk(username=params.get("USERNAME"), password=params.get("#PASSWORD"))
-        ttd_client.get_report_url(report_schedule_id=params.get("REPORT_SCHEDULE_ID"), advertiser_id=params.get("ADVERTISER_ID"))
+        ttd_client = TradeDesk(username=params.get("username"), password=params.get("#password"))
+        ttd_client.get_report_url(report_schedule_id=params.get("report_schedule_id"),
+                                  advertiser_id=params.get("advertiser_id"))
         data = ttd_client.get_data()
 
-        table = self.create_out_table_definition("output.csv", incremental=False)
-        out_table_path = table.full_path
+        is_incremental = bool(params.get("incremental_output"))
+
+        table = self.create_out_table_definition("output.csv", incremental=is_incremental,
+                                                 primary_key=params.get("primary_keys"))
 
         with open(table.full_path, mode="wt", encoding="utf-8", newline="") as out_file:
             out_file.write(data.text)
@@ -37,4 +40,3 @@ if __name__ == "__main__":
     except Exception as exc:
         logging.exception(exc)
         exit(2)
-    
