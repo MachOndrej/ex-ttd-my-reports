82db5af5836d8e5a3f1a879ad082aafae697ccdd
fcesnak <70899695+fcesnak@users.noreply.github.com>
2023-11-30 18:28:50 +0100

    feat: add logging

--- main.py
+++ main.py
@@ -10,15 +10,16 @@ class Component(ComponentBase):
 
     def __init__(self):
         super().__init__()
+        self._logger = logging.getLogger(__name__)
 
     def run(self):
         params = self.configuration.parameters
-        logging.info(params)
+        self._logger.debug(f"Running component with following params: {params}")
         ttd_client = TradeDesk(username=params.get("username"), password=params.get("#password"))
-        ttd_client.get_report_url(report_schedule_id=params.get("report_schedule_id"),
-                                  advertiser_id=params.get("advertiser_id"))
-        data = ttd_client.get_data()
-
+        report_url = ttd_client.get_report_url(report_schedule_id=params.get("report_schedule_id"),
+                                               advertiser_id=params.get("advertiser_id"))
+        data = ttd_client.get_data(report_url)
+        self._logger.info("Processing report.")
         is_incremental = bool(params.get("incremental_output"))
 
         table = self.create_out_table_definition("output.csv", incremental=is_incremental,
--- ttd.py
+++ ttd.py
@@ -1,11 +1,15 @@
 import json
+import logging
 
 import requests
+from requests.exceptions import HTTPError
+from keboola.component import UserException
 
 
 class TradeDesk:
 
-    def __init__(self, username: str, password: str) -> None:
+    def __init__(self, username: str, password: str, logger: logging.Logger = None) -> None:
+        self._logger = logger or logging.getLogger(__name__)
         self.username = username
         self.password = password
         self.session = requests.Session()
@@ -14,21 +18,32 @@ class TradeDesk:
 
     def _authenticate(self):
         auth_endpoint = "/authentication"
+        self._logger.debug("Authenticating")
         payload = {
             "Login": self.username,
             "Password": self.password
         }
         response = self.session.post(self.api_base_url+auth_endpoint, json=payload)
+        try:
+            response.raise_for_status()
+        except HTTPError as exc:
+            self._logger.error(f"Authentication failed: {exc}")
+            raise UserException(exc)
         response = response.json()
-        print(response)
-        token = response["Token"]
+        try:
+            token = response["Token"]
+            self._logger.debug("Token acquired successfully.")
+        except KeyError as exc:
+            self._logger.error(f"Failed to acquire token: {exc}")
+            raise UserException(exc)
         self.session.headers.update(
             {
                 "TTD-Auth": token
             }
         )
+        self._logger.info("Authentication successful.")
 
-    def get_report_url(self, report_schedule_id: str, advertiser_id: str):
+    def get_report_url(self, report_schedule_id: str, advertiser_id: str) -> str:
         report_url = "/myreports/reportexecution/query/advertisers"
         payload = {
             "AdvertiserIds": [advertiser_id],
@@ -45,11 +60,31 @@ class TradeDesk:
                 "Complete"
             ]
         }
+        self._logger.info(f"Retrieving scheduled report: {report_schedule_id}.")
         response = self.session.post(self.api_base_url+report_url, json=payload)
+        try:
+            response.raise_for_status()
+        except HTTPError as exc:
+            self._logger.error(f"Retrieval failed: {exc}")
+            raise UserException(exc)
         response = response.json()
-        self.download_url = response["Result"][0]["ReportDeliveries"][0]["DownloadURL"]
 
-    def get_data(self) -> requests.Response:
-        response = self.session.get(self.download_url)
+        try:
+            download_url = response["Result"][0]["ReportDeliveries"][0]["DownloadURL"]
+            self._logger.info("Report download URL acquired.")
+        except KeyError as exc:
+            self._logger.error(f"Download URL missing: {exc}.")
+            raise UserException(exc)
+
+        return download_url
+
+    def get_data(self, download_url: str) -> requests.Response:
+        self._logger.info("Retrieving report")
+        response = self.session.get(download_url)
+        try:
+            response.raise_for_status()
+        except HTTPError as exc:
+            self._logger.error(f"Report retrieval failed: {exc}")
+            raise UserException(exc)
 
         return response
