077ed5a47c99bfcd81521696e77bfe3d9c82cd89
fcesnak <70899695+fcesnak@users.noreply.github.com>
2023-12-10 13:05:07 +0100

    Refactor methods and add configuration options
    Introduced changes that enhance the functionality and flexibility of the app. The main codebase has been refactored to support multiple advertiser IDs in report retrieval. It now splits advertiser IDs from parameters and passes them as a list. Additionally, new configuration options are added and the 'keboola.utils' package is integrated into the project to facilitate data handling. A couple of JSON files were also added to configure properties.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from typing import List
 from keboola.component.base import ComponentBase, sync_action
 from keboola.component.sync_actions import SelectElement
 from keboola.component import UserException
+from keboola.utils.helpers import comma_separated_values_to_list
 
 from ttd import TradeDesk
 
@@ -19,14 +20,15 @@ class Component(ComponentBase):
         self._logger.debug(f"Running component with following params: {params}")
         ttd_client = TradeDesk(username=params.get("username"), password=params.get("#password"))
         advertisers = params.get("advertiser_ids")
+        table_name = params.get("output_table_name")
+        is_incremental = bool(params.get("incremental_output"))
         advertiser_ids = advertisers.split(",")
         report_url = ttd_client.get_report_url(report_schedule_id=params.get("report_schedule_id"),
                                                advertiser_ids=advertiser_ids)
         data = ttd_client.get_data(report_url)
         self._logger.info("Processing report.")
-        is_incremental = bool(params.get("incremental_output"))
 
-        table = self.create_out_table_definition("output.csv", incremental=is_incremental,
+        table = self.create_out_table_definition(f"{table_name}.csv", incremental=is_incremental,
                                                  primary_key=params.get("primary_keys"))
 
         with open(table.full_path, mode="wt", encoding="utf-8", newline="") as out_file:
@@ -34,14 +36,27 @@ class Component(ComponentBase):
 
         self.write_manifest(table)
 
-    @sync_action("get_advertisers")
-    def get_advertisers(self) -> List[SelectElement]:
+    def _get_advertisers(self) -> List[dict[str, str]]:
         params = self.configuration.parameters
-        ttd_client = TradeDesk(username=params.get("username"), password=params.get("#password"))
-        partners = ttd_client.get_partners()
-        advertisers = ttd_client.get_advertisers(partners)
+        if isinstance(params.get("advertiser_id", ""), str):
+            advertiser_ids = comma_separated_values_to_list(params.get("advertiser_id", ""))
+        else:
+            advertiser_ids = params.get("advertiser_id", [])
+        if not advertiser_ids:
+            ttd_client = TradeDesk(username=params.get("username"), password=params.get("#password"))
+            partners = ttd_client.get_partners()
+            advertiser_ids = ttd_client.get_advertisers(partners)
+
+            return advertiser_ids
+        if not advertiser_ids:
+            advertiser_ids = []
+        return advertiser_ids
+
+    @sync_action("load_advertisers")
+    def load_advertisers(self) -> List[SelectElement]:
+        advertiser_ids = self._get_advertisers()
         select_elements = [SelectElement(value=adv["advertiser_id"], label=adv["advertiser_name"]) for adv in
-                           advertisers]
+                           advertiser_ids]
 
         return select_elements
 
